900c0a4bf31bfef5712d10f68866bcc1744b079a
Micaela <luzmicaelaponce00@gmail.com>
2026-01-14 13:01:36 -0300

    cambiando configuracion

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* Opciones normales */
   serverExternalPackages: ["better-sqlite3"],
-
-  // @ts-ignore - Forzamos la desactivación de Turbo aunque el tipo proteste
-  turbo: {
-    enabled: false,
-  },
 };
 
 export default nextConfig;
