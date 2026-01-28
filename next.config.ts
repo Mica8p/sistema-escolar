cac1a0d0635bb337cbb945e477cc1ccf761e8845
Micaela <luzmicaelaponce00@gmail.com>
2026-01-28 13:46:57 -0300

    arreglando la configuración de next

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* Opciones normales */
   serverExternalPackages: ["better-sqlite3"],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '4mb',
+    },
+  },
 };
 
 export default nextConfig;
