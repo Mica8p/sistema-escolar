344a67e3b08907cdbc6b1bb64d397f231122a79c
Micaela <luzmicaelaponce00@gmail.com>
2026-03-16 20:07:46 -0300

    Arreglando la configuración de prisma

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
+
 const prismaClientSingleton = () => {
   const adapter = new PrismaBetterSqlite3({
     url: "file:./prisma/dev.db",
